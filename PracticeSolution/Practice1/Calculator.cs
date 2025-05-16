19f48f9d3bb0c1687bf4f5c20cc6ccc583431864
Serj <ssspirin_86@inbox.ru>
2025-05-17 00:04:20 +0300

    Added refactoring class

---
+++ PracticeSolution/Practice1/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRefactoring
+{
+    public class Calculator : Calculation
+    {
+        public object ConvertAndCalc(object x, object y)
+        {
+            object result = 0;
+
+            if (x != null && y != null && x is int && y is int)
+                result = (int)x + (int)y;
+
+            else
+                Console.WriteLine("One of the operands is null, or is not an integer");
+
+            return result;
+        }
+
+        public int Calc(string a, string b)
+        {
+            return base.Calc(a, b);
+        }
+
+        public void CalcData(string a, string b)
+        {
+            try
+            {
+                int val = Calc(a, b);
+                Console.WriteLine("Value: " + val);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+    public class Calculation
+    {
+        public int Calc(string a, string b)
+        {
+            return a.Length + b.Length;
+        }
+
+        public void PrintData(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
--- PracticeSolution/Practice1/Program.cs
+++ PracticeSolution/Practice1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Roman!");
+Console.WriteLine("Hello, World!");
